34b9bf402ba35d4e092d2710f4e50ebf2faf86e0
Kalan Prudhomme <cleaver9@gmail.com>
2021-04-06 15:08:50 -0700

    added chart, fixed page link, more styling

--- Results/results.js
+++ Results/results.js
@@ -3,14 +3,13 @@ const resultsTable = document.querySelector('table');
 
 const dex = getDex();
 
-for (let dexObject of dex) {
-    console.log(dexObject);
-
-    const resultsData = createTableRow(dexObject);
-    
-    resultsTable.append(resultsData);
-}
-
+//for (let dexObject of dex) {
+//    console.log(dexObject);
+//
+//    const resultsData = createTableRow(dexObject);
+//    
+//    resultsTable.append(resultsData);
+//}
 
 
 const resetButton = document.getElementById('reset');
@@ -19,6 +18,47 @@ const resetButton = document.getElementById('reset');
 resetButton.addEventListener('click', () => {
 
     localStorage.clear();
-    window.location = '../../index.html';
+    window.location = '../index.html';
 
+});
+
+
+const names = [];
+const captures = [];
+const encounters = [];
+
+for (let pokemon of dex) {
+    names.push(pokemon.id);
+    captures.push(pokemon.captured);
+    encounters.push(pokemon.encountered);
+}
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, { //eslint-disable-line
+    type: 'bar',
+    data: {
+        labels: names,
+        datasets: [{
+            label: 'Pokemon Captured',
+            data: captures,
+            backgroundColor: 'red',
+               
+            borderColor: 'pink',
+            borderWidth: 1
+        },
+        {
+            label: 'Pokemon Encountered',
+            data: encounters,
+            backgroundColor: 'blue',
+            borderColor: 'purple',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
 });
